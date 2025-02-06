18feb028c5c126fd450d3c82be548f75d53fb8b1
Stephen <866628@pdsb.net>
2025-02-06 17:59:02 -0500

    done roller, do pivot next, -SigmasUnite

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,4 +4,11 @@ public class Constants {
     public static class LoggedDashboard {
         public static boolean tuningMode = false;
     }
+    
+    public static class Mapping {
+      public static class Intake {
+      public static final int intakeMotor = 11;
+      public static final int leftBeamBreak = 5;
+      public static final int rightBeamBreak = 4;
+    }
 }
