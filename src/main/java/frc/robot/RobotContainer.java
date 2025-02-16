5219281a22ceacacc8fa828fef7165fe009ca209
racheld816 <racheldai0722@gmail.com>
2025-02-16 00:55:21 -0500

    shooter system

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,16 +5,59 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.commands.ShooterCommand;
+import frc.robot.commands.RotateShooterCommand;
+import frc.robot.subsystems.ShooterSubsystem;
+import frc.robot.subsystems.RotateShooterSystem;
 
-public class RobotContainer {
-  public RobotContainer() {
+public class RobotContainer
+{
+  private final RotateShooterSystem m_rotateShooterSystem;
+  private final ShooterSubsystem m_shooterSubsystem;
+
+  private final ShooterCommand shooterCommand;
+  private final RotateShooterCommand rotateShooterCommand;
+
+  public RobotContainer()
+  {
+    m_rotateShooterSystem = new RotateShooterSystem();
+    m_shooterSubsystem = new ShooterSubsystem();
+
+    shooterCommand = new ShooterCommand(m_shooterSubsystem, () -> getShooterControl());
+    m_shooterSubsystem.setDefaultCommand(shooterCommand);
+
+    rotateShooterCommand = new RotateShooterCommand(m_rotateShooterSystem, () -> getRotateShooterControl());
+    m_rotateShooterSystem.setDefaultCommand(rotateShooterCommand);
+
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings()
+  {
+    stick1.X.whileTrue(new RotateShooterCommand(m_rotateShooterSystem, () -> m_rotateShooterSystem.autoAlignShooter()));
+    stick1.X.whileTrue(new ShooterCommand(m_shooterSubsystem, () -> Constants.Shooter.shooterRPS));
+
+    stick1.A.whileTrue(new ShooterCommand(m_shooterSubsystem, () -> Constants.Shooter.shooterRPS));
+    stick1.A.whileTrue(new RotateShooterCommand(m_rotateShooterSystem, () -> Constants.GamePieces.speaker.angle));
+
+    stick1.Y.whileTrue(new ShooterCommand(m_shooterSubsystem, () -> 20.0));
+    stick1.Y.whileTrue(new RotateShooterCommand(m_rotateShooterSystem, () -> m_rotateShooterSystem.autoAlignShooter()));
+  }
+
+
+  private double getShooterControl()
+  {
+    return 0.0;
+  }
+
+  private double getRotateShooterControl()
+  {
+    return 0.0;
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
   }
+
 }
---
+++ src/main/java/frc/robot/ShooterCommand.java
@@ -0,0 +1,43 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.subsystems.ShooterSubsystem;
+import edu.wpi.first.wpilibj2.command.Command;
+
+import java.util.function.Supplier;
+
+public class ShooterCommand extends Command {
+  private final ShooterSubsystem m_subsystem;
+  private final Supplier<Double> control;
+
+  public ShooterCommand(ShooterSubsystem subsystem, Supplier<Double> control) {
+    m_subsystem = subsystem;
+    this.control = control;
+    addRequirements(subsystem);
+  }
+
+  @Override
+  public void initialize() {
+    m_subsystem.selectProfileSlot(0);
+    m_subsystem.stopControllers();
+  }
+
+  @Override
+  public void execute() {
+    double control = this.control.get();//converting to rpm
+    m_subsystem.setMotorOutput(control);
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    m_subsystem.stopControllers();
+  }
+
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
