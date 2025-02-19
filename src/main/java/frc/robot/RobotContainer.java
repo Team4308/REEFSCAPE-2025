13c55ab8fb651a0b6baf1b363e170e537422d08d
racheld816 <racheldai0722@gmail.com>
2025-02-19 14:31:00 -0500

    Revert "shooter system"
    This reverts commit 5219281a22ceacacc8fa828fef7165fe009ca209.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,59 +5,16 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import frc.robot.commands.ShooterCommand;
-import frc.robot.commands.RotateShooterCommand;
-import frc.robot.subsystems.ShooterSubsystem;
-import frc.robot.subsystems.RotateShooterSystem;
+import edu.wpi.first.wpilibj2.command.Commands;
 
-public class RobotContainer
-{
-  private final RotateShooterSystem m_rotateShooterSystem;
-  private final ShooterSubsystem m_shooterSubsystem;
-
-  private final ShooterCommand shooterCommand;
-  private final RotateShooterCommand rotateShooterCommand;
-
-  public RobotContainer()
-  {
-    m_rotateShooterSystem = new RotateShooterSystem();
-    m_shooterSubsystem = new ShooterSubsystem();
-
-    shooterCommand = new ShooterCommand(m_shooterSubsystem, () -> getShooterControl());
-    m_shooterSubsystem.setDefaultCommand(shooterCommand);
-
-    rotateShooterCommand = new RotateShooterCommand(m_rotateShooterSystem, () -> getRotateShooterControl());
-    m_rotateShooterSystem.setDefaultCommand(rotateShooterCommand);
-
+public class RobotContainer {
+  public RobotContainer() {
     configureBindings();
   }
 
-  private void configureBindings()
-  {
-    stick1.X.whileTrue(new RotateShooterCommand(m_rotateShooterSystem, () -> m_rotateShooterSystem.autoAlignShooter()));
-    stick1.X.whileTrue(new ShooterCommand(m_shooterSubsystem, () -> Constants.Shooter.shooterRPS));
-
-    stick1.A.whileTrue(new ShooterCommand(m_shooterSubsystem, () -> Constants.Shooter.shooterRPS));
-    stick1.A.whileTrue(new RotateShooterCommand(m_rotateShooterSystem, () -> Constants.GamePieces.speaker.angle));
-
-    stick1.Y.whileTrue(new ShooterCommand(m_shooterSubsystem, () -> 20.0));
-    stick1.Y.whileTrue(new RotateShooterCommand(m_rotateShooterSystem, () -> m_rotateShooterSystem.autoAlignShooter()));
-  }
-
-
-  private double getShooterControl()
-  {
-    return 0.0;
-  }
-
-  private double getRotateShooterControl()
-  {
-    return 0.0;
-  }
+  private void configureBindings() {}
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
   }
-
 }
