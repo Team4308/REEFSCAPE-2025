9d62fc3dae3b845fe087be6a4c22d13015e93578
Owen Wang <o0wang@outlook.com>
2025-02-27 16:09:04 -0500

    make branch

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,4 +4,8 @@ public class Constants {
     public static class LoggedDashboard {
         public static boolean tuningMode = false;
     }
+
+    public static class Mapping {
+        public static final int CcralRollerMotor = 0;
+    }
 }
---
+++ src/main/java/frc/robot/subsystems/CoralRollerSubsystem.java
@@ -0,0 +1,50 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import ca.team4308.absolutelib.wrapper.MotoredSubsystem;
+import edu.wpi.first.util.sendable.Sendable;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
+
+public class CoralRollerSubsystem extends MotoredSubsystem {
+    public final TalonFX motor;
+    final VelocityVoltage velocity = new VelocityVoltage(0);
+    final DutyCycleOut motorOut = new DutyCycleOut(0);
+    final TalonFXConfiguration config = new TalonFXConfiguration();
+
+    public CoralRollerSubsystem() {
+        motor = new TalonFX(Constants.Mapping.CoralRollerMotor);
+        motor.setNeutralMode(NeutralModeValue.Brake);
+
+        config.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+        config.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+
+        motor.getConfigurator().apply(config);
+    }
+
+    @Override
+    public void stopControllers() {
+        motorOut.Output = 0;
+        motor.setControl(motorOut);
+    }
+
+    public void setOutput(double rps) {
+        motor.setControl(velocity.withVelocity(rps));
+    }
+
+    @Override
+    public Sendable log() {
+        return this;
+    }
+
+    public Command runRollerCommand(double rps) {
+        return this.run(() -> setOutput(rps));
+    }
+    
+}
