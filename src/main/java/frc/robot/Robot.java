983f952e295f818f2f0391f8d5de6f969bd1ac60
racheld816 <racheldai0722@gmail.com>
2025-02-27 20:55:28 -0500

    end effector

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,65 +8,83 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import ca.team4308.absolutelib.control.XBoxWrapper;
+
 public class Robot extends TimedRobot {
-  private Command m_autonomousCommand;
 
-  private final RobotContainer m_robotContainer;
+  public final TalonFX right;
+  public final TalonFX left;
 
-  public Robot() {
-    m_robotContainer = new RobotContainer();
+  public final XBoxWrapper button;
+
+  final VelocityVoltage rightVelocity;
+  final VelocityVoltage leftVelocity;
+
+  button = new XBoxWrapper(0);
+
+  public Robot() { 
+
   }
-
-  @Override
-  public void robotPeriodic() {
-    CommandScheduler.getInstance().run();
-  }
-
-  @Override
-  public void disabledInit() {}
-
-  @Override
-  public void disabledPeriodic() {}
-
-  @Override
-  public void disabledExit() {}
-
-  @Override
-  public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.schedule();
-    }
-  }
-
-  @Override
-  public void autonomousPeriodic() {}
-
-  @Override
-  public void autonomousExit() {}
 
   @Override
   public void teleopInit() {
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
-    }
+
+    right = new TalonFX(0);
+    left = new TalonFX(1);
+
+    rightVelocity = new VelocityVoltage(0);
+        leftVelocity = new VelocityVoltage(0);
+
+        var slot0Configs = new Slot0Configs();
+        slot0Configs.kV = 0.12;
+        slot0Configs.kP = 0;
+        slot0Configs.kI = 0;
+        slot0Configs.kD = 0;
+
+        rightConfiguration = new TalonFXConfiguration();
+        leftConfiguration = new TalonFXConfiguration();
+
+        rightConfiguration.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+        rightConfiguration.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+        leftConfiguration.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+        leftConfiguration.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+
+        right.getConfigurator().apply(rightConfiguration);
+        left.getConfigurator().apply(leftConfiguration);
+
+        right.getConfigurator().apply(slot0Configs, Constants.Generic.timeoutMs);
+        left.getConfigurator().apply(slot0Configs, Constants.Generic.timeoutMs);
+
   }
 
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+
+    double pressed1 = res(button.getRightY());
+    double pressed2 = res(button.getLeftY());
+
+    rightVelocity.Slot = 0;
+        leftVelocity.Slot = 0;
+        right.setControl(rightVelocity.withVelocity(15*pressed1));
+        left.setControl(leftVelocity.withVelocity(15*pressed2));
+  }
+
+  double res(double num){
+    if(num<0.05&&num>-0.05){
+      return 0;
+    }
+    return num;
+  }
 
   @Override
   public void teleopExit() {}
 
-  @Override
-  public void testInit() {
-    CommandScheduler.getInstance().cancelAll();
-  }
-
-  @Override
-  public void testPeriodic() {}
-
-  @Override
-  public void testExit() {}
 }
