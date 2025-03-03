df8945c50820f2ec2f0d8609f09deceb7ee1aaf3
bluefeng2 <57335144+bluefeng2@users.noreply.github.com>
2025-03-03 10:53:46 -0500

    removed armfeedforward from swerve subsystem

--- src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
@@ -20,7 +20,6 @@ import com.pathplanner.lib.util.swerve.SwerveSetpointGenerator;
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
-import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
