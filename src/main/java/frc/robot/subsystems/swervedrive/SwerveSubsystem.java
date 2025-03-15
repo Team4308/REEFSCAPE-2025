f0f3207fae09768e0cbcb12d4ead3c8b7d555af4
unknown <nguyenmax2008@gmail.com>
2025-03-15 11:44:39 -0400

    tuned align we're so back

--- src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
@@ -296,8 +296,8 @@ public class SwerveSubsystem extends SubsystemBase {
   public Command driveToPose(Pose2d pose) {
     // Create the constraints to use while pathfinding
     PathConstraints constraints = new PathConstraints(
-        swerveDrive.getMaximumChassisVelocity() * 0.2, 2.0,
-        swerveDrive.getMaximumChassisAngularVelocity() * 0.25, Units.degreesToRadians(720));
+        swerveDrive.getMaximumChassisVelocity(), 1.5,
+        swerveDrive.getMaximumChassisAngularVelocity(), Units.degreesToRadians(360));
 
     // Since AutoBuilder is configured, we can use it to build pathfinding commands
     return AutoBuilder.pathfindToPose(
