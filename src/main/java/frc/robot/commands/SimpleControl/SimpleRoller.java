2a969cac4de83f13e90f91c0f8075b3c5acc1406
bluefeng2 <57335144+bluefeng2@users.noreply.github.com>
2025-03-15 21:13:45 -0500

    manual overrides commands sometimes

--- src/main/java/frc/robot/commands/SimpleControl/SimpleRoller.java
+++ src/main/java/frc/robot/commands/SimpleControl/SimpleRoller.java
@@ -3,7 +3,9 @@ package frc.robot.commands.SimpleControl;
 
 import java.util.function.Supplier;
 
+import ca.team4308.absolutelib.control.XBoxWrapper;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Ports;
 import frc.robot.subsystems.CoralRollerSubsystem;
 
 public class SimpleRoller extends Command {
@@ -33,7 +35,8 @@ public class SimpleRoller extends Command {
 
     @Override
     public boolean isFinished() {
-        return subsystem.beamBreak.get();
+        return (subsystem.beamBreak.get()) || (new XBoxWrapper(Ports.Joysticks.OPERATOR).getLeftTrigger()
+                + new XBoxWrapper(Ports.Joysticks.OPERATOR).getRightTrigger() > 0.15);
     }
 
 }
