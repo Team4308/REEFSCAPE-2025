64d297834f13f5936aa81fed5772d850948dd4b9
unknown <nguyenmax2008@gmail.com>
2025-03-17 19:10:23 -0400

    on field calibration elevator preset changes

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -103,11 +103,11 @@ public final class Constants {
         public static double MIN_HEIGHT = Units.inchesToMeters(4.875);
 
         // Preset heights in inches
-        public static final double L1 = Units.inchesToMeters(6.5);
+        public static final double L1 = Units.inchesToMeters(6.0);
         public static final double L2 = Units.inchesToMeters(17.0);
         public static final double L3 = Units.inchesToMeters(33.0);
-        public static final double ALGAE1 = Units.inchesToMeters(33.0);
-        public static final double ALGAE2 = Units.inchesToMeters(46.875);
+        public static final double ALGAE1 = Units.inchesToMeters(30.0);
+        public static final double ALGAE2 = Units.inchesToMeters(46.0);
 
         // Speed constants (in meters per second)
         public static final double ALGAE_REMOVAL_SPEED = 1;
