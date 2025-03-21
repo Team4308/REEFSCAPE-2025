e50aea60c3e00f4ca9755361bbce2fb60f852c60
bluefeng2 <57335144+bluefeng2@users.noreply.github.com>
2025-03-21 09:42:35 -0500

    better dual cycle

--- src/main/java/frc/robot/commands/ScoreAndRemove.java
+++ src/main/java/frc/robot/commands/ScoreAndRemove.java
@@ -1,6 +1,5 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.ElevatorSubsystem;
@@ -14,17 +13,17 @@ import frc.robot.subsystems.AlgaeArmSubsystem;
 import frc.robot.subsystems.CoralRollerSubsystem;
 
 public class ScoreAndRemove extends SequentialCommandGroup {
-    public ScoreAndRemove(ElevatorSubsystem elevatorSubsystem, CoralRollerSubsystem rollerSubsystem,
-            AlgaeArmSubsystem algaeArmSubsystem) {
-        addCommands(
-                new SimpleAlgae(() -> constEndEffector.algaePivot.MAX_ANGLE, algaeArmSubsystem),
-                new SimpleElevator(() -> constElevator.L2, elevatorSubsystem),
-                new SimpleRoller(() -> constEndEffector.rollerSpeeds.L23, rollerSubsystem),
-                new SimpleAlgae(() -> constEndEffector.algaePivot.REMOVAL_ANGLE_BOTTOM, algaeArmSubsystem),
-                new ParallelDeadlineGroup(
-                        new SimpleElevator(() -> constElevator.ALGAE1, elevatorSubsystem),
-                        new DefaultRoller(() -> -constEndEffector.rollerSpeeds.ALGAE_REMOVAL,
-                                rollerSubsystem)),
-                new SimpleAlgae(() -> constEndEffector.algaePivot.MAX_ANGLE, algaeArmSubsystem));
-    }
+        public ScoreAndRemove(ElevatorSubsystem elevatorSubsystem, CoralRollerSubsystem rollerSubsystem,
+                        AlgaeArmSubsystem algaeArmSubsystem) {
+                addCommands(
+                                new SimpleAlgae(() -> constEndEffector.algaePivot.REMOVAL_ANGLE_BOTTOM,
+                                                algaeArmSubsystem),
+                                new SimpleElevator(() -> constElevator.L2, elevatorSubsystem),
+                                new SimpleRoller(() -> constEndEffector.rollerSpeeds.L23, rollerSubsystem),
+                                new ParallelDeadlineGroup(
+                                                new SimpleElevator(() -> constElevator.ALGAE1, elevatorSubsystem),
+                                                new DefaultRoller(() -> -constEndEffector.rollerSpeeds.ALGAE_REMOVAL,
+                                                                rollerSubsystem)),
+                                new SimpleAlgae(() -> constEndEffector.algaePivot.MAX_ANGLE, algaeArmSubsystem));
+        }
 }
