078c629e74aa376e242a169013f6613ed92e0794
gaocan_wang <117655210+GaocanWang@users.noreply.github.com>
2025-03-23 11:07:07 -0400

    tolreance change

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -33,11 +33,11 @@ public final class Constants {
 
         public static final class Align {
             public static final class Heading {
-                public static final double TOLERANCE = 1.0; // in degrees
+                public static final double TOLERANCE = 2.0; // in degrees
             }
 
             public static final class Translation {
-                public static final double TOLERANCE = Units.inchesToMeters(0.5);
+                public static final double TOLERANCE = 0.02; // in meters
             }
         }
 
