50407266f8374607793169dd1b5b1260e62088da
gaocan_wang <117655210+GaocanWang@users.noreply.github.com>
2025-04-02 15:24:31 -0400

    cooked

--- src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
@@ -373,7 +373,7 @@ public class SwerveSubsystem extends SubsystemBase {
 
     // Create the constraints to use while pathfinding
     PathConstraints constraints = new PathConstraints(
-        swerveDrive.getMaximumChassisVelocity(), 2.0,
+        swerveDrive.getMaximumChassisVelocity(), 3.0,
         swerveDrive.getMaximumChassisAngularVelocity(), Units.degreesToRadians(360));
 
     // Create the goal state
