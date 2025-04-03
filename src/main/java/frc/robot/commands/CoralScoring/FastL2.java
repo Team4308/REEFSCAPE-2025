b8ab50c2995c7d4f28bf59c5a6d393d87f8e0636
gaocan_wang <117655210+GaocanWang@users.noreply.github.com>
2025-04-03 09:47:48 -0400

    auton change

--- src/main/java/frc/robot/commands/CoralScoring/FastL2.java
+++ src/main/java/frc/robot/commands/CoralScoring/FastL2.java
@@ -4,8 +4,8 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.ElevatorSubsystem;
 import frc.robot.Constants.constEndEffector;
 import frc.robot.Constants.constElevator;
-import frc.robot.commands.SimpleControl.SimpleAlgae;
-import frc.robot.commands.SimpleControl.SimpleElevator;
+import frc.robot.commands.SimpleControl.SimpleAlgaeTimeout;
+import frc.robot.commands.SimpleControl.SimpleElevatorTimeout;
 import frc.robot.commands.SimpleControl.SimpleRoller;
 import frc.robot.subsystems.AlgaeArmSubsystem;
 import frc.robot.subsystems.CoralRollerSubsystem;
@@ -14,10 +14,10 @@ public class FastL2 extends SequentialCommandGroup {
     public FastL2(ElevatorSubsystem elevatorSubsystem, CoralRollerSubsystem rollerSubsystem,
             AlgaeArmSubsystem algaeArmSubsystem) {
         addCommands(
-                new SimpleAlgae(() -> constEndEffector.algaePivot.REST_ANGLE,
+                new SimpleAlgaeTimeout(() -> constEndEffector.algaePivot.REST_ANGLE,
                         algaeArmSubsystem),
-                new SimpleElevator(() -> constElevator.L2, elevatorSubsystem),
+                new SimpleElevatorTimeout(() -> constElevator.L2, elevatorSubsystem),
                 new SimpleRoller(() -> constEndEffector.rollerSpeeds.L23, rollerSubsystem),
-                new SimpleElevator(() -> constElevator.MIN_HEIGHT, elevatorSubsystem));
+                new SimpleElevatorTimeout(() -> constElevator.MIN_HEIGHT, elevatorSubsystem));
     }
 }
--- src/main/java/frc/robot/commands/CoralScoring/FastL3.java
+++ src/main/java/frc/robot/commands/CoralScoring/FastL3.java
@@ -4,8 +4,8 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.ElevatorSubsystem;
 import frc.robot.Constants.constEndEffector;
 import frc.robot.Constants.constElevator;
-import frc.robot.commands.SimpleControl.SimpleAlgae;
-import frc.robot.commands.SimpleControl.SimpleElevator;
+import frc.robot.commands.SimpleControl.SimpleAlgaeTimeout;
+import frc.robot.commands.SimpleControl.SimpleElevatorTimeout;
 import frc.robot.commands.SimpleControl.SimpleRoller;
 import frc.robot.subsystems.AlgaeArmSubsystem;
 import frc.robot.subsystems.CoralRollerSubsystem;
@@ -14,9 +14,9 @@ public class FastL3 extends SequentialCommandGroup {
     public FastL3(ElevatorSubsystem elevatorSubsystem, CoralRollerSubsystem rollerSubsystem,
             AlgaeArmSubsystem algaeArmSubsystem) {
         addCommands(
-                new SimpleAlgae(() -> constEndEffector.algaePivot.REST_ANGLE, algaeArmSubsystem),
-                new SimpleElevator(() -> constElevator.L3, elevatorSubsystem),
+                new SimpleAlgaeTimeout(() -> constEndEffector.algaePivot.REST_ANGLE, algaeArmSubsystem),
+                new SimpleElevatorTimeout(() -> constElevator.L3, elevatorSubsystem),
                 new SimpleRoller(() -> constEndEffector.rollerSpeeds.L23, rollerSubsystem),
-                new SimpleElevator(() -> constElevator.MIN_HEIGHT, elevatorSubsystem));
+                new SimpleElevatorTimeout(() -> constElevator.MIN_HEIGHT, elevatorSubsystem));
     }
 }
